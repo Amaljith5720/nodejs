8f3de1d0491af22a6f5e50bd18868b0a1adfa4e5
Amaljith5720 <133842950+Amaljith5720@users.noreply.github.com>
2023-05-17 10:50:48 +0530

    nodejs examples

---
+++ nodejs/nodeexample/app.js
@@ -0,0 +1,13 @@
+// var a = 2                                ----> To add 2 numbers 
+// var b = 2
+// var c = a+b
+// console.log("Amaljith  " + c )
+
+
+
+// var http = require ('http')
+                                       
+// http.createServer(function(req,res){          ----> To run a service
+//     res.write("Welcome Amaljith")
+//     res.end()
+// }).listen(8080)
